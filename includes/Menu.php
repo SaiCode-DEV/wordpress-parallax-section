ecb17b17a096277019819c339a268ac4ea80d332
SaiCode <talentix.loopingflieger@gmail.com>
2025-11-02 15:55:33 +0100

    Remove Payed Garbage

--- includes/Menu.php
+++ includes/Menu.php
@@ -6,54 +6,27 @@ class Menu {
 	public function __construct() {
 		add_action( 'admin_menu', [ $this, 'adminMenu' ] );
 		add_action( 'admin_enqueue_scripts', [$this, 'adminEnqueueScripts'] );
-		$this->isUserPremium = psIsPremium();
 	}
 
 	public function adminMenu() {
-
-		if ($this->isUserPremium)  {
-			add_menu_page(
-				        __('Parallax Section', 'parallax-section'), // Page title
-				        __('Parallax Section', 'parallax-section'), // Menu title
-				        'manage_options',                           // Capability
-				        'parallax-section-dashboard',               // Menu slug
-				        [$this, 'renderDashboardPage'],             // Callback function
-				        'dashicons-images-alt2',                    // Icon (Dashicons or URL)
-				        20                                          // Position
-				    );
-		}else {
-			add_submenu_page(
-				'tools.php',
-				__('Parallax Section', 'parallax-section'),
-				__('Parallax Section', 'parallax-section'),
-				'manage_options',
-				'parallax-section-dashboard',
-				[$this, 'renderDashboardPage'],
-				0
-			);
-		}
+		add_menu_page(
+			__('Parallax Section', 'parallax-section'), // Page title
+			__('Parallax Section', 'parallax-section'), // Menu title
+			'manage_options',                           // Capability
+			'parallax-section-dashboard',               // Menu slug
+			[$this, 'renderDashboardPage'],             // Callback function
+			'dashicons-images-alt2',                    // Icon (Dashicons or URL)
+			20                                          // Position
+		);
 	}
-
-// 	public function adminMenu() {
-//     add_menu_page(
-//         __('Parallax Section', 'parallax-section'), // Page title
-//         __('Parallax Section', 'parallax-section'), // Menu title
-//         'manage_options',                           // Capability
-//         'parallax-section-dashboard',               // Menu slug
-//         [$this, 'renderDashboardPage'],             // Callback function
-//         'dashicons-images-alt2',                    // Icon (Dashicons or URL)
-//         20                                          // Position
-//     );
-// }
-
 
 	public function renderDashboardPage(){ ?>
 		<div
 			id='apbDashboard'
 			data-info='<?php echo esc_attr( wp_json_encode( [
 				'version' => PSB_VERSION,
-				'isPremium' => psIsPremium(),
-				'hasPro' => PARALLAX_HAS_PRO
+				'isPremium' => true,
+				'hasPro' => true
 			] ) ); ?>'
 		></div>
 	<?php }
